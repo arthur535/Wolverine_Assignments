838c3c3183381f34c9800fa0b9b816880cf11dc0
Heisenberg <109768662+arthur535@users.noreply.github.com>
2022-09-27 21:39:56 +0400

    Add files via upload

---
+++ 27.09.22/lowercase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+void toLowerCase(std::string&);
+
+int main(){
+    std::string name;
+    name = "valAR MarghulIS";
+    toLowerCase(name);
+    std::cout << name << std::endl;
+}
+
+void toLowerCase(std::string& str) {
+    for (int i = 0; i < str.size(); ++i) {
+        if (str[i] >= 91 && str[i] <= 122) {
+            str[i] -= 32;
+        }
+    }
+}
